9d35f756bc807a8e61087a1327394560d4d00189
itsdevnate <jonathankalu313@gmail.com>
2025-06-09 15:15:55 +0100

    feat: Add fetch request to retrieve and log data from JSONPlaceholder API

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -203,7 +203,7 @@ function getTotalReviewCount(book) {
 }
 console.log(getTotalReviewCount(book));
 */
-
+/*
 function getTotalReviewCount(book) {
   const goodreads = book.reviews.goodreads.reviewsCount;
   const librarything = book.reviews.librarything?.reviewsCount ?? 0;
@@ -263,3 +263,8 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
   book.id === 1 ? { ...book, pages: 1210 } : book
 );
 booksAfterUpdate;
+*/
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
