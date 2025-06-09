7219a59e900ff0d7e13f4efcf7ce57ccc045d229
itsdevnate <jonathankalu313@gmail.com>
2025-06-09 13:32:22 +0100

    fix: Ensure librarything reviews count defaults to 0 in getTotalReviewCount function

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -197,7 +197,7 @@ console.log(book.translations.spanish);
 
 function getTotalReviewCount(book) {
   const goodreads = book.reviews.goodreads.reviewsCount;
-  const librarything = book.reviews.librarything?.reviewsCount;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
   return goodreads + librarything;
 }
 console.log(getTotalReviewCount(book));
