608be1fbbfa6ce7de22152847229521921d25f83
itsdevnate <jonathankalu313@gmail.com>
2025-06-12 20:53:25 +0100

    feat: Add async function to fetch and log todos from JSONPlaceholder API

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -264,7 +264,15 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 );
 booksAfterUpdate;
 */
-
+/*
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((res) => res.json())
   .then((data) => console.log(data));
+  */
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+getTodos();
